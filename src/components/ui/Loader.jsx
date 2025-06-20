a6109db891a4a8c3e9a4ee58d2c8242c4459fada
swapnil77122 <swapnildhage7885@gmail.com>
2025-06-20 09:38:31 +0530

    f

--- src/components/ui/Loader.jsx
+++
@@ -1,5 +0,0 @@
-const Loader = () => (
-  <div className="text-center py-20 text-xl text-blue-500">Loading...</div>
-);
-
-export default Loader;
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { FilterProvider } from './contexts/FilterContext';
+
 import 'leaflet/dist/leaflet.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <FilterProvider>
+    
         <App />
-      </FilterProvider>
-    </ThemeProvider>
+      
   </React.StrictMode>
 );
